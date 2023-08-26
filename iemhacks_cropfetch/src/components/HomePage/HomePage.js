77f6deb996425b61c3c8194dfeff75e5e7545cf3
shreyas <shreyas.mohanty03@gmail.com>
2023-08-26 19:07:34 +0530

    updates

---
+++ iemhacks_cropfetch/src/components/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <Fragment>
+            <div className="home-top-box">
+                <div className='home-img'>
+                    <h1 data-aos="fade-up" data-aos-duration="1000" >Project: Krishi</h1>
+                    <div className="overlay"></div>
+                </div>
+            </div>
+        </Fragment>
+
+    )
+}
+
+export default HomePage
+
